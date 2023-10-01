5fade10d084358f65780142db3b40a44fbc82493
mubarizahmed <mebza@live.com>
2023-10-01 20:52:47 +0200

    fix: font import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,9 @@ import { Footer, Navbar } from '../components'
 import { Space_Grotesk } from 'next/font/google';
 import '@/styles/globals.css'
 
+const sg = Space_Grotesk({subsets: ['latin']});
+
 export default function App({ Component, pageProps }) {
-
-  const sg = Space_Grotesk({ subsets: ['latin'] })
-
   return (
     <div className={"dotted__bg " + sg.className}>
       <Navbar />
